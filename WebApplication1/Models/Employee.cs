60c687a3d9329d1818755442850810bce0e4b733
srinu <srinu@SRINIVAS_ROY>
2022-03-16 18:23:10 +0530

    Add project files.

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+
+        [Key]
+        public string? Emp_ID { get; set; }
+        public string? Emp_Name { get; set; }
+        public string? Designation { get; set; }
+        public string? Location { get; set; }
+        public DateTime Doj { get; set; }
+        public DateTime Lwd { get; set; }
+       public string? Vertical_L2 { get; set; }
+        public string? Email_ID { get; set; }
+        public string? Rm_Emp_ID { get; set; }
+        public string? Band { get; set; }
+        public string? Level { get; set; }
+
+
+
+    }
+}
---
+++ WebApplication1/Models/WebApplication1Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class WebApplication1Context : DbContext
+    {
+        public WebApplication1Context()
+        {
+        }
+
+        public WebApplication1Context(DbContextOptions<WebApplication1Context> options)
+            : base(options)
+        {
+        }
+
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+    }
+}
