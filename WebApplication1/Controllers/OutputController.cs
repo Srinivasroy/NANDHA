ade2d5b25773e6855ad54eb664c4e2f16c102fd4
Renuga Devi <96512353+renu-121@users.noreply.github.com>
2022-03-17 16:47:28 +0530

    new controller

---
+++ WebApplication1/Controllers/OutputController.cs
@@ -0,0 +1,51 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OutputEmpController : ControllerBase
+    {
+        private readonly WebApplication1Context _context;
+
+        public OutputEmpController(WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        // Post: api/OutputEmp
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee(Employee employee)
+        {
+
+            string StoredProc = "exec GetdetByID " +
+            "@e_ID = '" + employee.Emp_ID + "'," +
+            "@Emp_Name = '" + employee.Emp_Name + "'," +
+            "@Designation= '" + employee.Designation + "'," +
+            "@Location= '" + employee.Location + "'," +
+            "@DOJ= '" + employee.Doj + "'," +
+            "@LWD= '" + employee.Lwd + "'," +
+            "@Vertical_L2= '" + employee.Vertical_L2 + "'," +
+            "@Email_ID= '" + employee.Email_ID + "'," +
+            "@RM_Emp_ID= '" + employee.Rm_Emp_ID + "'," +
+            "@Band= '" + employee.Band + "'," +
+            "@Level= '" + employee.Level + "'," +
+            "@Role_ID= '" + employee.Role_ID + "'"
+             ;
+
+
+            return await _context.Employee.FromSqlRaw(StoredProc).ToListAsync();
+            //return await _context.Employee.ToListAsync();
+        }
+
+
+    }
+}
--- WebApplication1/Models/Employee.cs
+++ WebApplication1/Models/Employee.cs
@@ -7,19 +7,57 @@ namespace WebApplication1.Models
     {
 
         [Key]
+        [Required]
+        [Display(Name = "Employee ID")]
         public string? Emp_ID { get; set; }
-        public string? Emp_Name { get; set; }
-        public string? Designation { get; set; }
-        public string? Location { get; set; }
-        public DateTime Doj { get; set; }
-        public DateTime Lwd { get; set; }
-       public string? Vertical_L2 { get; set; }
-        public string? Email_ID { get; set; }
-        public string? Rm_Emp_ID { get; set; }
-        public string? Band { get; set; }
-        public string? Level { get; set; }
-        public string? Role_ID { get; set; }
 
+
+        [Required(ErrorMessage = "Name Required")]
+        [Display(Name = "Employee Name")]
+        public string? Emp_Name { get; set; }
+
+
+        [Required]
+        public string? Designation { get; set; }
+        [Required(ErrorMessage = "Location Required")]
+        public string? Location { get; set; }
+
+
+        [Required]
+        [Display(Name = "Date of Joining")]
+        public DateTime Doj { get; set; }
+
+
+        
+        [Display(Name = "Last Working Day")]
+        public DateTime Lwd { get; set; }
+
+
+        [Required]
+        [Display(Name = "Vertical L2")]
+        public string? Vertical_L2 { get; set; }
+
+
+
+        [Required(ErrorMessage = "Email Required")]
+        [Display(Name = "Email ID")]
+        public string? Email_ID { get; set; }
+
+
+        [Required]
+        public string? Rm_Emp_ID { get; set; }
+
+
+        [Required]
+        public string? Band { get; set; }
+
+
+        [Required]
+        public string? Level { get; set; }
+
+
+        [Required]
+        public string? Role_ID { get; set; }
 
 
     }
